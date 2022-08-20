cf6f5e27f483a6cb7087b297efe102a92e8d90c0
vishnu244 <vishnunali244@gmail.com>
2022-08-20 12:47:40 +0530

    [Vishnu]-added SignUp Component

--- fundoo/src/App.js
+++ fundoo/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Signin from './pages/signin/signin';
+import Signup from './pages/signup/signup';
 
 
 function App() {
   return (
     <div className="App">
-      <Signin></Signin>
+      {/* <Signin/> */}
+      <Signup/>
     </div>
   );
 }
