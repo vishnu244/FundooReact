d15b8cd469a535d19080a69a26732150b3b1c577
vishnu244 <vishnunali244@gmail.com>
2022-08-22 15:14:30 +0530

    [Vishnu]-added takeNote2 Component

--- fundoo/src/App.js
+++ fundoo/src/App.js
@@ -5,7 +5,7 @@ import Signup from './pages/signup/signup';
 import Header from './Components/Header/header';
 import TakeNote1 from './Components/TakeNote1/takenote1';
 import TakeNote2 from './Components/TakeNote2/takenote2';
-
+import Head from './Components/TakeNote2/takenote2';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       {/* <Header /> */}
       {/* <TakeNote1/> */}
       <TakeNote2/>
+  
     </div>
   );
 }
