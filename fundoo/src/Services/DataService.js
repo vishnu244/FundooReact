0f0efdfc6ee66c4ab6d96200f2e1cd6b935cbf24
vishnu244 <vishnunali244@gmail.com>
2022-09-06 07:58:46 +0530

    [vishnu]-Final Commit with Modifications

--- fundoo/src/Services/DataService.js
+++ fundoo/src/Services/DataService.js
@@ -6,8 +6,10 @@ let header = {
     }
 }
 
+
 export const getNotes = () => {
-    let response = axios.post('http://127.0.0.1:8000/api/jointables', header);
+    // let response = axios.post('http://127.0.0.1:8000/api/jointables',header);
+    let response = axios.get('http://127.0.0.1:8000/api/displayNotes',header);
     return response;
 }
 
@@ -16,13 +18,18 @@ export const addColor = (colorNotesObj) => {
     return response;
 }
 
-export const addTrash = (trashNotesObj) => {
+export const trashNotes = (trashNotesObj) => {
     let response = axios.post('http://127.0.0.1:8000/api/TrashNotesById', trashNotesObj, header);
     return response;
 }
 
-export const addNote = (takeNote2Obj) => {
-    let response = axios.post('http://127.0.0.1:8000/api/Notes', takeNote2Obj, header);
+export const archiveNotes = (archiveNotesObj) => {
+    let response = axios.post('http://127.0.0.1:8000/api/ArchieveNotesById', archiveNotesObj, header);
+    return response;
+}
+
+export const addNote = (noteObj) => {
+    let response = axios.post('http://127.0.0.1:8000/api/Notes', noteObj, header);
     return response;
 }
 
@@ -31,7 +38,3 @@ export const updateNotes = (updateNotesObj) => {
     return response;
 }
 
-export const archiveNotes = (archiveNotesObj) => {
-    let response = axios.post('http://127.0.0.1:8000/api/ArchieveNotesById', archiveNotesObj, header);
-    return response;
-}
